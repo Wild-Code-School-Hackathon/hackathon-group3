c9c76013af24228721b7fb4c2a217c29c4e405f4
Tiago Baptista Coelho <tiagojnbaptistacoelho@gmail.com>
2020-04-29 18:32:05 +0100

    added random-cam feature to randomCam component

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import RandomCam from './RandomCam'
 
 function Homepage() {
     return(
-        <div />
+        <RandomCam />
     );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,8 @@
 import React from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-} from 'react-router-dom';
-
 
 function Navbar() {
     return (
-        <div>
-            <NavBar>
-                {/** Placeholder for the NavBar*/}
-            NavBar
-        </NavBar>
-            <Content>
-                {
-                    /** show as content whatever we provide */
-                    props.children
-                }
-            </Content>
-        </div>;
+       <div />
     );
 }
 

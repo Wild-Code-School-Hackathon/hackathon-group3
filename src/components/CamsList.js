84a8e7996463627b5b983ec5c8a435eb307fde33
Tiago Baptista Coelho <tiagojnbaptistacoelho@gmail.com>
2020-04-30 12:52:17 +0100

    latest adds

--- src/components/CamsList.js
+++ src/components/CamsList.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 function CamsList(props) {
-    const { name } = props
+    const { name, id, selectCam } = props
     console.log(name)
     return (
         <div>
-            <p>{name}</p>
+            <p onClick={() => selectCam(id)}><Link to='/webcamPage'>{name}</Link></p>
         </div>
     )
 }
--- src/components/WebcamPage.js
+++ src/components/WebcamPage.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import axios from 'axios';
 
-function WebcamPage() {
-    return(
-        <div>Webcam Page</div>
-    );
+
+
+class WebcamPage extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            camInfo: [],
+        }
+    }
+    componentDidMount() {
+        const { location } = this.props;
+        const { camId } = location.state;
+        const url = `https://api.windy.com/api/webcams/v2/list/webcam=${camId}?show=webcams:player,location&key=EjDBdKjXSKLRgpbFwOcQh2N6MbS8S3Ym`
+        axios
+            .get(url)
+            .then((response) => response.data.result.webcams[0])
+            .then((chosenCamInfo) => {
+                this.setState({ camInfo: chosenCamInfo }) 
+            })
+    }
+    render() {
+        const { camInfo } = this.state;
+        console.log(camInfo)
+        return (
+            <div>
+                <h3>{camInfo.title}</h3>
+               {camInfo.location && 
+               <h3>{`${camInfo.location.city}, ${camInfo.location.country}`}</h3>}
+               {camInfo.player &&
+                 <iframe src={camInfo.player.lifetime.embed} title={camInfo.title}/>
+               }
+            </div >
+        );
+    }
 }
 
 export default WebcamPage;
